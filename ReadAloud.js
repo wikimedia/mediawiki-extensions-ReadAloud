169d2d3183e2c89f0acc6e331d184981426facf1
Sophivorus <schenonef@gmail.com>
2023-09-04 06:23:02 -0700

    Initial commit
    Change-Id: Ifff3e80c19066f0cbf5eb6cb201a4bf169f156a9

---
+++ ReadAloud.js
@@ -0,0 +1,115 @@
+window.ReadAloud = {
+
+	/**
+	 * Initialization script
+	 */
+	init: function () {
+		ReadAloud.bind();
+
+		// Stop any running voice
+		window.speechSynthesis.cancel();
+	},
+
+	/**
+	 * Bind events
+	 */
+	bind: function () {
+		$( '#ca-read-aloud' ).on( 'click', ReadAloud.readAloud );
+		$( '#ca-pause-reading' ).on( 'click', ReadAloud.pauseReading );
+	},
+
+	/**
+	 * Read the current page aloud
+	 */
+	readAloud: function () {
+
+		// Swap buttons
+		$( this ).hide();
+		$( '#ca-pause-reading' ).show();
+
+		// If speech was paused, just resume
+		if ( window.speechSynthesis.paused ) {
+			window.speechSynthesis.resume();
+			return;
+		}
+
+		// Only read selected elements
+		var $content = $( '#mw-content-text .mw-parser-output' );
+		var $elements = $content.children( 'h1, h2, h3, h4, h5, h6, p, ul, ol' );
+		$elements.addClass( 'read' ).on( 'click', ReadAloud.jumpToElement );
+		ReadAloud.readNextElement();
+	},
+
+	index: 0,
+	readNextElement: function () {
+		var $element = $( '.read' ).eq( ReadAloud.index );
+		ReadAloud.index++;
+		$( '.reading' ).removeClass( 'reading' );
+		$element.addClass( 'reading' );
+		var text = $element.text();
+		text = text.replace( / ([A-Z])\./g, ' $1' ); // Remove dots from acronyms to prevent confusion with sentences
+		text = text.replace( /[([].*?[\])]/g, '' ); // Don't read parentheses
+		var sentences = text.split( '. ' ); // Include space to prevent matching things like "99.9%"
+		sentences = sentences.filter( s => s ); // Remove empty sentences
+		ReadAloud.sentences = sentences;
+		ReadAloud.readNextSentence();
+	},
+
+	sentences: [],
+	readNextSentence: function () {
+		var sentence = ReadAloud.sentences.shift();
+		var utterance = new window.SpeechSynthesisUtterance( sentence );
+		utterance.lang = mw.config.get( 'wgPageContentLanguage' );
+		window.speechSynthesis.cancel();
+		window.speechSynthesis.speak( utterance );
+		utterance.onend = ReadAloud.onUtteranceEnd;
+	},
+
+	/**
+	 * @note This will fire not only when the utterance finishes
+	 * but also when the speechSynthesis is cancelled
+	 * notably when jumping to another element
+	 * This is why we need the ugly skipNextSentence hack seen below
+	 */
+	onUtteranceEnd: function () {
+		if ( ReadAloud.skipNextSentence ) {
+			ReadAloud.skipNextSentence = false;
+			return;
+		}
+		if ( ReadAloud.sentences.length ) {
+			ReadAloud.nextSentenceTimeout = setTimeout( ReadAloud.readNextSentence, 500 );
+			return;
+		}
+		if ( ReadAloud.index < $( '.read' ).length ) {
+			ReadAloud.nextElementTimeout = setTimeout( ReadAloud.readNextElement, 1000 );
+			return;
+		}
+	},
+
+	/**
+	 * Jump to a specific element
+	 */
+	nextElementTimeout: null,
+	nextSentenceTimeout: null,
+	jumpToElement: function () {
+		var $element = $( this );
+		var index = $( '.read' ).index( $element );
+		ReadAloud.index = index;
+		ReadAloud.skipNextSentence = true;
+		ReadAloud.readNextElement();
+	},
+
+	/**
+	 * Pause reading aloud
+	 */
+	pauseReading: function () {
+		// Swap buttons
+		$( this ).hide();
+		$( '#ca-read-aloud' ).show();
+
+		// Pause reading
+		window.speechSynthesis.pause();
+	}
+};
+
+$( ReadAloud.init );
---
+++ ReadAloud.php
@@ -0,0 +1,53 @@
+<?php
+
+class ReadAloud {
+
+	/**
+	 * @param OutputPage &$out
+	 * @param Skin &$skin
+	 */
+	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
+		$out->addModules( 'ext.ReadAloud' );
+		$out->addModuleStyles( 'ext.ReadAloud.styles' );
+	}
+
+	/**
+	 * @param SkinTemplate $skinTemplate
+	 * @param array &$links
+	 */
+	public static function onSkinTemplateNavigationUniversal( SkinTemplate $skinTemplate, array &$links ) {
+		global $wgReadAloudNamespaces;
+
+		// Only add the buttons in relevant pages
+		$skin = $skinTemplate->getSkin();
+		$title = $skin->getTitle();
+		if ( ! $title->exists() ) {
+			return;
+		}
+		$context = $skin->getContext();
+		$action = Action::getActionName( $context );
+		if ( $action !== 'view' ) {
+			return;
+		}
+		$namespace = $title->getNamespace();
+		if ( ! in_array( $namespace, $wgReadAloudNamespaces ) ) {
+			return;
+		}
+
+		// Define the buttons
+		$readAloud = [
+			'id' => 'ca-read-aloud',
+			'href' => '#',
+			'text' => wfMessage( 'readaloud-read-aloud' )->plain()
+		];
+		$pauseReading = [
+			'id' => 'ca-pause-reading',
+			'href' => '#',
+			'text' => wfMessage( 'readaloud-pause-reading' )->plain()
+		];
+
+		// Add the buttons
+		$links['actions']['read-aloud'] = $readAloud;
+		$links['actions']['pause-reading'] = $pauseReading;
+	}
+}
